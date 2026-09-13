5bee555f08e2313d300d7b23f141180bd64ffb85
prospjx <prospergodsson@gmail.com>
2026-09-12 21:22:28 -0400

    Enable EKS cluster resource and remove obsolete myfile

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -22,24 +22,24 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
   role       = aws_iam_role.cluster.name
 }
 
-# resource "aws_eks_cluster" "myeks" {
-#   name = "myeks"
-# 
-#   access_config {
-#     authentication_mode = "API"
-#   }
-# 
-#   role_arn = aws_iam_role.cluster.arn
-#   version  = "1.31"
-# 
-#   vpc_config {
-#     subnet_ids = var.subnet_ids
-#   }
-# 
-#   # Ensure that IAM Role permissions are created before and deleted
-#   # after EKS Cluster handling. Otherwise, EKS will not be able to
-#   # properly delete EKS managed EC2 infrastructure such as Security Groups.
-#   depends_on = [
-#     aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
-#   ]
-# }
+resource "aws_eks_cluster" "myeks" {
+  name = "myeks"
+
+  access_config {
+    authentication_mode = "API"
+  }
+
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.31"
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted
+  # after EKS Cluster handling. Otherwise, EKS will not be able to
+  # properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+  ]
+}
