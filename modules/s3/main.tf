916d4aefba3d7a21e2504ae139c057ef6b63ddf9
prospjx <prospergodsson@gmail.com>
2026-09-19 18:33:17 -0400

    Changed the s3 website configuration

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -8,25 +8,13 @@ resource "aws_s3_bucket" "sbucket" {
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "website" {
-  bucket = aws_s3_bucket.sbucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
 resource "aws_s3_bucket_public_access_block" "access" {
   bucket = aws_s3_bucket.sbucket.id
 
   block_public_acls       = true
-  block_public_policy     = false
+  block_public_policy     = true
   ignore_public_acls      = true
-  restrict_public_buckets = false
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "version" {
